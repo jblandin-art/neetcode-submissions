62bc83b90aab554d22b45229eb6deb2bf26cdfe1
jblandin-art <josiahblanding@gmail.com>
2026-07-14 14:45:55 -0400

    Add: valid-sudoku - submission-84

---
+++ Data Structures & Algorithms/valid-sudoku/submission-84.py
@@ -0,0 +1,53 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # O(n)
+        def alldiff(nums):
+            if len(nums) == 0:
+                return True
+            hashmap = defaultdict(int)
+            for num in nums:
+                hashmap[num] += 1
+            del hashmap['.']
+            for value in hashmap.values():
+                if value > 1:
+                    return False
+            return True
+        
+        # check if row is alldiff
+        # O(n^2)
+        for row in board:
+            if alldiff(row) == False:
+                print("row failed", row)
+                return False
+        # check if column is alldiff
+        # O(n^2)
+        column = []
+        for c in range(len(board)):
+            for r in range(len(board)):
+                column.append(board[r][c])
+            print(column)
+            if alldiff(column) == False:
+                print("column failed", row)
+                return False
+            column = []
+
+        # check if square is alldiff
+        # O (n^2)
+        square = []
+        for c in range(len(board) // 3):
+            for r in range(len(board)):
+                if (r % 3 + c*3) % 3 == 0:
+                    if alldiff(square) == False:
+                        print("square failed", row)
+                        return False 
+                    square = []
+                    print("switch")
+                #print(r % 3 + c*3)
+                print(r)
+                square.extend(board[r % 3 + c*3][r // 3 * 3:r // 3 * 3 + 3])
+                print("board square", square)
+        
+        return True
+
+        return 
+    
